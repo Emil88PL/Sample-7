7e1a1071e4037e3607d331c977d2b00286ddae11
Emil <emil88pl@gmail.com>
2016-08-29 14:31:49 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,4 +2,18 @@
 $(function(){
 
     $("#myCarousel").hide(20).slideDown(400);
+
 });
+
+
+// Good way
+
+$(function(){
+
+    $(".panel-button").on("click", function(){
+       var panelId = $(this).attr("data-panelid");
+       $("#"+panelId).toggle();
+    });
+
+});
+
